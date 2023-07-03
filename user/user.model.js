0c88fc2ec22d844fed4031399aea19ce3d1c16dd
Varaprasad Reddy <varaprasadreddy.polu@gmail.com>
2023-07-03 08:40:31 +0530

    Removed DB connection.

--- user/user.model.js
+++ user/user.model.js
@@ -1,28 +1,29 @@
 //Import Modules or Paths
-import Sequelize from 'sequelize'
-import sequelize from "../utility/rds.sequelize.js"
+import Sequelize from "sequelize";
+import sequelize from "../utility/rds.sequelize.js";
+
 //Create a Schema
-const UserSchema = sequelize.define('User', {
-    id: {
-        type: Sequelize.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
-    },
-    name: {
-        type: Sequelize.STRING,
-        allowNull: false
-    },
-    email: {
-        type: Sequelize.STRING,
-        allowNull: false,
-        required: true,
-        unqiue: true
-    },
-    password: {
-        type: Sequelize.STRING,
-        required: true,
-        allowNull: false
-    }
+const UserSchema = sequelize.define("User", {
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    required: true,
+    unqiue: true,
+  },
+  password: {
+    type: Sequelize.STRING,
+    required: true,
+    allowNull: false,
+  },
 });
 //Export user schema
 export default UserSchema;
--- utility/rds.sequelize.js
+++ utility/rds.sequelize.js
@@ -1,26 +1,28 @@
-//Import Modules or Paths 
-import Sequelize from 'sequelize'
-import dotenv from 'dotenv'
-dotenv.config()
+//Import Modules or Paths
+import Sequelize from "sequelize";
+import dotenv from "dotenv";
+dotenv.config();
+import mysql from "mysql2";
+
+const connection = mysql.createConnection(process.env.DATABASE_URL);
 //sequelize connection setup for mariadb
-const sequelize = new Sequelize(process.env.DB_NAME || 'User', process.env.DB_USERNAME, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
-  dialect: 'mysql',
-  operatorsAliases: false,
-  pool: {
-    max: 3,
-    min: 2,
-    acquire: 30000,
-    idle: 10000
-  }
+const sequelize = new Sequelize({
+  dialect: "mysql",
+  dialectModule: mysql,
+  define: {
+    timestamps: false,
+  },
+  connection: connection,
 });
+
 // Sequelize Sync
-sequelize.sync({ force: false })
+sequelize
+  .sync({ force: false })
   .then(() => {
-    console.log(`Database & tables created!`)
+    console.log(`Database & tables created!`);
   })
-  .catch(error => {
-    console.log("Error is................", error)
-  })
-//Export sequelize connection                        
+  .catch((error) => {
+    console.log("Error is................", error);
+  });
+//Export sequelize connection
 export default sequelize;
